92e655231fc7cc351b95a1b312b5fbb4605a6da2
Yasen-Getzov-09 <yasen.getzov@icloud.com>
2025-04-27 22:35:19 +0300

    Moving to Fullscreen

--- sideways_shooter.py
+++ sideways_shooter.py
@@ -18,8 +18,9 @@ class SidewaysShooter:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-                (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Sideways Shooter")
 
         # Create an instance to store game statistics.
